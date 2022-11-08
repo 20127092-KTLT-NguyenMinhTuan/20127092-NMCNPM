c7e2cc0da7a09a44fe14afe175f6526fa604ac12
NguyenMinhTuan <minhtuan251102@gmail.com>
2022-11-08 22:10:28 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Iron Man");
+    searchMovies("");
   }, []);
 
   const searchMovies = async (title) => {
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>Sury Movie</h1>
 
       <div className="search">
         <input
@@ -31,11 +31,8 @@ const App = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for movies"
         />
-        <img
-          src={'https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F0%2F0b%2FSearch_Icon.svg%2F1024px-Search_Icon.svg.png&imgrefurl=https%3A%2F%2Fvi.m.wikipedia.org%2Fwiki%2FT%25E1%25BA%25ADp_tin%3ASearch_Icon.svg&tbnid=kKn-48JDx0tqQM&vet=12ahUKEwiB-8Cawpz7AhXRSfUHHQ0qD2IQMygAegUIARC2AQ..i&docid=NJACVTI_1WjQkM&w=1024&h=1024&q=icon%20search&client=opera-gx&ved=2ahUKEwiB-8Cawpz7AhXRSfUHHQ0qD2IQMygAegUIARC2AQ'}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
+        <button
+          onClick={() => searchMovies(searchTerm)}> Search</button>
       </div>
 
       {movies?.length > 0 ? (
@@ -46,7 +43,8 @@ const App = () => {
         </div>
       ) : (
         <div className="empty">
-          <h2>No movies found</h2>
+          <br></br>
+          <img src='https://cdn.dribbble.com/userupload/2905354/file/original-92212c04a044acd88c69bedc56b3dda2.png?compress=1&resize=752x' alt="No results found"></img>
         </div>
       )}
     </div>
