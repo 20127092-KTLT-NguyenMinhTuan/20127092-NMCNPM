e38b661e08e1d4e6fca4bfdf776d13a2f1c8d145
NguyenMinhTuan <minhtuan251102@gmail.com>
2022-11-10 20:58:03 +0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,41 @@
 import React, { useState, useEffect } from "react";
 
-import MovieCard from "./MovieCard";
-
 import "./App.css";
 
 const API_URL = "http://www.omdbapi.com?apikey=7f52e152";
 
-const App = () => {
+function App () {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
 
   useEffect(() => {
     searchMovies("");
   }, []);
-
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
-  };
+  
+  const MovieCard = ({ movie: { imdbID, Poster, Title } }) => {
+    return (
+      <div className="movie" key={imdbID}>
+          <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+      </div>
+    );
+  }
 
   return (
     <div className="app">
-      <h1>Sury Movie</h1>
-
+      <h1>Movie Heaven</h1>
       <div className="search">
         <input
+          className="inputcss search"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={(temp) => setSearchTerm(temp.target.value)}
           placeholder="Search for movies"
         />
-        <button
+        <button 
           onClick={() => searchMovies(searchTerm)}> Search</button>
       </div>
 
@@ -42,7 +46,7 @@ const App = () => {
           ))}
         </div>
       ) : (
-        <div className="empty">
+        <div className="NotFound">
           <br></br>
           <img src='https://cdn.dribbble.com/userupload/2905354/file/original-92212c04a044acd88c69bedc56b3dda2.png?compress=1&resize=752x' alt="No results found"></img>
         </div>
