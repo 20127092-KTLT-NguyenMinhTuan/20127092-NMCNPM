06c9b019d8277b3905379cc5f65d99c361718bc6
NguyenMinhTuan <minhtuan251102@gmail.com>
2022-12-09 22:08:41 +0700

    Update App.js

--- search-item/src/App.js
+++ search-item/src/App.js
@@ -4,23 +4,24 @@ import { useState } from "react";
 
 function App() {
   const [searchTerm,setSearchTerm] = useState('')
+  
   return(
     <div className="App">
       <input type = "text" placeholder="Search.." onChange={event => {setSearchTerm(event.target.value)}}></input>
       {jsondata.filter((val)=>{
-        if (searchTerm == "") {
-          return val
-        } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
-            return val
-        }
-      }).map((val,key)=> {
-        return (
-        <div className="User" key={key}>
-          <p>{val.first_name}</p>
-        </div>
-        );
-      })
-      }
+    if (searchTerm == "") {
+      return val
+    } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val
+    }
+  }).map((val,key)=> {
+    return (
+    <div className="User" key={key}>
+      <p>{val.first_name}</p>
+    </div>
+    );
+  })
+  }
     </div>
   )
 }
